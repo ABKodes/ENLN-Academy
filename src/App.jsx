22bbddbcf670052d6ac1ab8c97291c031e043e28
ABKodes <abiyshiferaw12@gmail.com>
2023-12-21 11:52:17 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import "./App.css";
+import HeaderBefore from "./components/Reusable Components/HeaderBefore";
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <>
-      <h1 className="herotext">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HeaderBefore/>
   );
 }
 

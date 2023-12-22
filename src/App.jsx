f07ebe7f486e83cf106d77a8af2c72128ff3f72d
BettyyN <betelhemnegash10@gmail.com>
2023-12-22 14:51:38 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import HeaderBefore from "./components/Reusable Components/HeaderBefore";
+//import HeaderBefore from "./components/Reusable Components/HeaderBefore";
+//import HeaderGov from "./components/Reusable Components/Header Gov";
+import HeaderOrg from "./components/Reusable Components/Header Org";
 function App() {
   return (
-    <HeaderBefore/>
+    // <HeaderBefore/>
+    // <HeaderGov/>
+    <HeaderOrg/>
   );
 }
 
--- src/components/Reusable Components/Header Gov.jsx
+++ src/components/Reusable Components/Header Gov.jsx
@@ -1,29 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 const HeaderGov = () => {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
     <>
-      <div className="scroll-hidden -mx-3 overflow-y-auto whitespace-nowrap bg-accent px-4 py-3">
-        <a
-          className="smalltext mx-4 transform uppercase leading-5 text-secondary duration-300 hover:underline dark:text-secondary md:my-0"
-          href="#"
-        >
+      <div className="overflow-y-auto whitespace-nowrap bg-accent px-4 py-3">
+        <Link className="smalltext group relative mx-4 transform uppercase leading-5 text-secondary duration-300 dark:text-secondary md:my-0">
           For individuals
-        </a>
-        <a
-          className="smalltext mx-4 transform uppercase leading-5 text-secondary duration-300 hover:underline dark:text-secondary md:my-0"
-          href="#"
-        >
-          for government
-        </a>
-        <a
-          className="smalltext mx-4 transform uppercase leading-5 text-secondary duration-300 hover:underline dark:text-secondary md:my-0"
-          href="#"
-        >
-          for organization
-        </a>
+          <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-secondary transition-transform group-hover:scale-x-100"></span>
+        </Link>
+        <Link className="smalltext group relative mx-4 transform uppercase leading-5 text-secondary duration-300 dark:text-secondary md:my-0">
+          For government
+          <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-secondary transition-transform group-hover:scale-x-100"></span>
+        </Link>
+        <Link className="smalltext group relative mx-4 transform uppercase leading-5 text-secondary duration-300 dark:text-secondary md:my-0">
+          For organization
+          <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-secondary transition-transform group-hover:scale-x-100"></span>
+        </Link>
       </div>
       <div className="navbar bg-secondary">
         <div className="navbar-start">
@@ -37,24 +32,28 @@ const HeaderGov = () => {
           </Link>
         </div>
         <div className="navbar-center  hidden lg:flex">
-          <div className=" menu  menu-horizontal space-x-5">
-           
-              <Link className="group relative  text-primary hover:font-bold">
-                About Us
-                <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-primary transition-transform group-hover:scale-x-100"></span>
-              </Link>
-           
-              <Link className="group relative text-primary hover:font-bold">
-                Why Us
-                <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-primary transition-transform group-hover:scale-x-100"></span>
-              </Link>
-            
+          <div className=" menu  menu-horizontal space-x-8">
+            <Link className="group relative  text-primary hover:font-bold">
+              About Us
+              <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-primary transition-transform group-hover:scale-x-100"></span>
+            </Link>
+
+            <Link className="group relative text-primary hover:font-bold">
+              Why Us
+              <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-primary transition-transform group-hover:scale-x-100"></span>
+            </Link>
           </div>
         </div>
-        <div className="navbar-end">
-          <Link className="btn mr-8 rounded-sm border-none bg-slate-300 px-2 py-0 text-white">
+        <div className="navbar-end mr-8">
+          <motion.a
+            initial={{ scale: 0.8 }}
+            whileHover={{ scale: 0.9 }}
+            whileTap={{ scale: 0.8 }}
+            transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            className="btn btn-primary"
+          >
             Contact Sales
-          </Link>
+          </motion.a>
         </div>
       </div>
     </>

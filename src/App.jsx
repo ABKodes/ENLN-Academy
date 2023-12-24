a1e2a86e20d0126924fd44dbcd495175b99446b4
ABKodes <abiyshiferaw12@gmail.com>
2023-12-24 22:58:32 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Footer from "./components/Reusable Components/Footer";
 import FooterGov from "./components/Reusable Components/FooterGov";
 import FooterOrg from "./components/Reusable Components/FooterOrg";
-//import HeaderBefore from "./components/Reusable Components/HeaderBefore";
+import HeaderBefore from "./components/Reusable Components/HeaderBefore";
 import HeaderGov from "./components/Reusable Components/Header Gov";
 function App() {
   return (
     <>
       <HeaderGov />
+      <HeaderBefore/>
       <br />
       <Footer />
       <br />

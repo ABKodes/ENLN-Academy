53cba0374e19631f3a35d40b69e6120b326a4dfa
BettyyN <betelhemnegash10@gmail.com>
2023-12-24 16:21:27 +0300

    configuration

--- src/components/Reusable Components/Header Gov.jsx
+++ src/components/Reusable Components/Header Gov.jsx
@@ -31,26 +31,6 @@ const HeaderGov = () => {
       <div className="navbar bg-slate-300">
         <div className="navbar-start">
           <div className="dropdown">
-            {/* <div
-               tabIndex={0}
-               role="button"
-               className="btn btn-ghost lg:hidden"
-             >
-               <svg
-                 xmlns="http://www.w3.org/2000/svg"
-                 className="h-5 w-5"
-                 fill="none"
-                 viewBox="0 0 24 24"
-                 stroke="currentColor"
-               >
-                 <path
-                   strokeLinecap="round"
-                   strokeLinejoin="round"
-                   strokeWidth="2"
-                   d="M4 6h16M4 12h8m-8 6h16"
-                 />
-               </svg>
-             </div> */}
             <div
               tabIndex={0}
               role="button"
@@ -63,7 +43,7 @@ const HeaderGov = () => {
                   className="h-6 w-6 text-white"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke="currentColor"
+                  stroke="#025464"
                 >
                   <path
                     strokeLinecap="round"
@@ -78,7 +58,7 @@ const HeaderGov = () => {
                   className="h-6 w-6 text-white"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke="currentColor"
+                  stroke="#025464"
                 >
                   <path
                     strokeLinecap="round"

d3725a9b9838f1a15be525c0f72f4210809ad5c7
ABKodes <abiyshiferaw12@gmail.com>
2023-12-27 09:11:52 +0300

    footer individual

--- src/components/Reusable Components/Footer.jsx
+++ src/components/Reusable Components/Footer.jsx
@@ -8,16 +8,6 @@ import { RiTwitterXLine } from "react-icons/ri";
 import { FaYoutube } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 const Footer = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
   return (
     <footer className="bg-accent">
       <footer className="mx-auto w-11/12 bg-accent uppercase">
@@ -95,70 +85,12 @@ const Footer = () => {
             <div>
               <p className="font-bold text-secondary">our socials</p>
 
-              <div className="mt-5 flex flex-col items-start space-y-2">
+              <div className="mt-5 flex flex-row items-start space-y-2 md:flex-col ">
                 <div className="grid grid-cols-3 gap-4">
                   {/* First Row */}
-                  <div className="wrapper">
-                    <a
-                      href="https://instagram.com/"
-                      target="_instagram"
-                      aria-label="A great place to find out what friends are up to!"
-                      onMouseEnter={handleMouseEnter}
-                      onMouseLeave={handleMouseLeave}
-                    >
-                      <div className="bg">
-                        <div className="inner">
-                          <FaInstagram className="text-2xl" />
-                        </div>
-                      </div>
-                    </a>
-                    <style jsx>{`
-                      a {
-                        color: #000;
-                        margin-right: 10px;
-                        position: relative;
-                        display: inline-block;
-                        font-size: 50px;
-                      }
-
-                      .inner {
-                        background: #fcfcfc;
-                        mix-blend-mode: screen;
-                        display: block;
-                        width: 43.75px;
-                      }
-
-                      .bg {
-                        width: calc(100% - 1px);
-                        background: #000;
-                        text-align: center;
-                        display: block;
-                        height: 50px;
-                        position: relative;
-                      }
-
-                      .bg::before {
-                        content: "";
-                        background: linear-gradient(
-                          45deg,
-                          #f09433 0%,
-                          #e6683c 25%,
-                          #dc2743 50%,
-                          #cc2366 75%,
-                          #bc1888 100%
-                        );
-                        display: block;
-                        position: absolute;
-                        top: 0;
-                        left: 0;
-                        height: 100%;
-                        width: 100%;
-                        opacity: ${isHovered ? "1" : "0"};
-                        transition: 0.3s ease-in-out;
-                      }
-                    `}</style>
+                  <div>
+                    <PiInstagramLogoThin className="text-3xl text-secondary" />
                   </div>
-
                   <div>
                     <PiTelegramLogoThin className="text-3xl text-secondary" />
                   </div>
@@ -168,7 +100,7 @@ const Footer = () => {
 
                   {/* Second Row */}
                   <div>
-                    <RiFacebookCircleLine className="text-3xl text-secondary hover:rounded-full hover:bg-blue-800" />
+                    <RiFacebookCircleLine className="text-3xl text-secondary" />
                   </div>
                   <div>
                     <PiLinkedinLogoLight className="text-3xl text-secondary" />
@@ -185,15 +117,11 @@ const Footer = () => {
 
           <div className="flex flex-col items-center justify-between sm:flex-row">
             <a href="#">
-              <img
-                className="h-10"
-                src="/public/footer individual.png"
-                alt=""
-              />
+              <img className="h-10" src="/public/footer individual.png" alt="" />
             </a>
 
             <p className="mt-4 text-sm text-secondary sm:mt-0">
-              © Copyright 2023. All Rights Reserved.
+              © Copyright 2021. All Rights Reserved.
             </p>
           </div>
         </div>

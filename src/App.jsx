064303b53d83bbe385946d61eab3476311656e61
ABKodes <abiyshiferaw12@gmail.com>
2023-12-28 09:22:37 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import Footer from "./components/Reusable Components/Footer";
 import FooterGov from "./components/Reusable Components/FooterGov";
 import FooterOrg from "./components/Reusable Components/FooterOrg";
-import HeaderBefore from "./components/Reusable Components/HeaderBefore";
-import HeaderGov from "./components/Reusable Components/Header Gov";
-import Hero from "./components/Home Ind/Hero";
+import HeaderAfter from "./components/Reusable Components/HeaderAfter";
 import HeaderLesson from "./components/Reusable Components/Header Lesson";
+import Hero from "./components/Home Ind/Hero"
 function App() {
   return (
     <>
@@ -14,8 +13,8 @@ function App() {
       <br />
       <br />
       <br />
-      {/* <HeaderBefore /> */}
-      {/* <Hero /> */}
+      <HeaderAfter />
+      <Hero />
       <br />
       <Footer />
       <br />

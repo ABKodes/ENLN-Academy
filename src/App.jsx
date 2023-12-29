8b325abf6737e2183713991d8fc18393f1aeecea
BettyyN <betelhemnegash10@gmail.com>
2023-12-29 10:44:49 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import FooterOrg from "./components/Reusable Components/FooterOrg";
 import HeaderAfter from "./components/Reusable Components/HeaderAfter";
 import HeaderLesson from "./components/Reusable Components/Header Lesson";
 import Hero from "./components/Home Ind/Hero";
+import VerticalCard from "./components/Reusable Components/VerticalCard";
 // import Companies from "./components/Home Ind/Companies";
+import CardDescription from "./components/Reusable Components/Card Description";
+
 function App() {
   return (
     <>
       <HeaderLesson />
       <br />
+      <VerticalCard />
+      <br />
+      <CardDescription />
       <br />
       <br />
-      <br />
-      <HeaderAfter />
-      <Hero />
+      {/* <HeaderAfter /> */}
+      {/* <Hero /> */}
       <br />
       <Footer />
       <br />

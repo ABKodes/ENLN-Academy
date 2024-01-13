bf892f9d7eecfb8325d49c588b1426a82960ba15
ABKodes <abiyshiferaw12@gmail.com>
2024-01-13 16:20:03 +0300

    Centered text fixed

--- src/components/Home Ind/Companies.jsx
+++ src/components/Home Ind/Companies.jsx
@@ -27,8 +27,8 @@ const LOGOS = [
 const Companies = () => {
   return (
     <div className="relative m-auto w-full overflow-hidden bg-white before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-[''] grid grid-cols-3">
-        <div className="w-full h-full bg-secondary bg-opacity-30 flex justify-center items-center mx-auto z-10 absolute">
-          <p className="herotext">Companies we've taught</p>
+        <div className="w-full h-full bg-secondary bg-opacity-20 flex justify-center items-center mx-auto z-10 absolute">
+          <p className="herotext dark:text-primary dark:font-bold">Companies we've taught</p>
         </div>
       <div className="animate-infinite-slider flex w-[calc(250px*10)]">
         {LOGOS.map((logo, index) => (

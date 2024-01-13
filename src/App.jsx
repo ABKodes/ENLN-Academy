bd16bae124ecd993a6c23da5ffb00169d82d7acb
BettyyN <betelhemnegash10@gmail.com>
2024-01-13 12:40:08 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,6 @@ import FilterSidebar from "./components/Categories/FilterSidebar";
 import CourseSidebar from "./components/Course Description/CourseSidebar";
 import CourseDetail from "./components/Course Description/CourseDetail";
 import WhatYouLearn from "./components/Course Description/WhatYouLearn";
-const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
-const truncateLength = 100;
 function App() {
   return (
     <>
@@ -60,9 +58,6 @@ function App() {
         <HeaderBefore />
         <CourseHero />
         <CourseSidebar />
-        <div className="container mx-auto p-4">
-          <CourseDetail text={longText} truncateLength={truncateLength} />
-        </div>
         <WhatYouLearn />
         <br />
         <Login />

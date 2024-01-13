a6c52dd1f11a47d386bcc820f7ead13b71dec00f
ABKodes <abiyshiferaw12@gmail.com>
2024-01-13 16:19:14 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,8 @@ import Benefits from "./components/Home Ind/Benefits";
 import ReviewModal from "./components/Reusable Components/Review Modal";
 import HorizontalCard from "./components/Reusable Components/HorizontalCard";
 import CourseSidebar from "./components/Course Description/CourseSidebar";
-import CourseDetail from "./components/Course Description/CourseDetail";
 import WhatYouLearn from "./components/Course Description/WhatYouLearn";
-const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
-const truncateLength = 100;
+import CourseContent from "./components/Course Description/CourseContent";
 function App() {
   return (
     <>
@@ -46,10 +44,8 @@ function App() {
         <HeaderBefore />
         <CourseHero />
         <CourseSidebar />
-        <div className="container mx-auto p-4">
-          <CourseDetail text={longText} truncateLength={truncateLength} />
-        </div>
         <WhatYouLearn />
+        <CourseContent />
         <br />
         <Login />
         <HorizontalCard />

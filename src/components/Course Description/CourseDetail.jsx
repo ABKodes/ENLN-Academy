fcf28dd5f9617db87f048793c0e13b0850febe07
ABKodes <abiyshiferaw12@gmail.com>
2024-01-17 08:41:53 +0300

    made it simple

--- src/components/Course Description/CourseDetail.jsx
+++ src/components/Course Description/CourseDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+
 function CourseDetail() {
   const longText =
     "Before embarking on this learning journey, ensure that you have the prerequisites to maximize engagement and success in the course. You should be comfortable using a computer, navigating software, and browsing the internet. A reliable internet connection is essential for accessing course materials and online resources. Whether using a computer, laptop, tablet, or smartphone, ensure your device has modern browsers for compatibility. Familiarity with common productivity tools like word processors and PDF readers is necessary. Proficiency in the language of instruction is vital for comprehending lectures and written materials. Dedicate sufficient time for reading, watching videos, and completing assignments according to the course schedule. Have note-taking tools, either digital or traditional, ready to capture key concepts. Approach the course with an open mindset, engaging with diverse perspectives and actively participating in discussions and collaborative projects. Intrinsic motivation and a positive learning attitude are key. Check for any hardware requirements specified in the course description and ensure familiarity with prerequisite subjects, if applicable. Enroll or have access to the designated learning platform for course materials and communication. For any questions or concerns about the requirements, feel free to reach out to the course instructor or support team for assistance.";
@@ -17,18 +18,14 @@ function CourseDetail() {
         <p className={`text py-5 ${showFullText ? "block" : "truncate"}`}>
           {longText}
         </p>
-        <p className={`heading ${showFullText ? "block" : "hidden"}`}>
-          Description
-        </p>
-        <p className={`text py-5 ${showFullText ? "block" : "hidden"}`}>
-          {longText}
-        </p>
-        <p className={`heading ${showFullText ? "block" : "hidden"}`}>
-          Who is this Course for
-        </p>
-        <p className={`text py-5 ${showFullText ? "block" : "hidden"}`}>
-          {longText}
-        </p>
+        {showFullText ? (
+          <>
+            <p className="heading">Description</p>
+            <p className="text py-5">{longText}</p>
+            <p className="heading">Who is this Course for</p>
+            <p className="text py-5">{longText}</p>
+          </>
+        ) : null}
       </div>
       <button
         className="group relative transform cursor-pointer leading-5 text-primary duration-300 dark:text-primary md:my-0"

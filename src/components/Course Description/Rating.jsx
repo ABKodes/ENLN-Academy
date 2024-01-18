867b4d29d4d5245ef43d7890d98803894a2373e5
ABKodes <abiyshiferaw12@gmail.com>
2024-01-18 09:01:48 +0300

    rating

---
+++ src/components/Course Description/Rating.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RatingCard from '../Reusable Components/Rating Card'
+function Rating() {
+  return (
+    <div className=''>
+        <RatingCard/>
+        <RatingCard/>
+    </div>
+  )
+}
+
+export default Rating

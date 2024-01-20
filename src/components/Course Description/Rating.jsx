06280034455a0a1b90a61eecfa36614037d08f52
ABKodes <abiyshiferaw12@gmail.com>
2024-01-20 22:38:27 +0300

    remade the card component

--- src/components/Course Description/Rating.jsx
+++ src/components/Course Description/Rating.jsx
@@ -147,6 +147,104 @@ const Rating = () => {
             </div>
           </div>
         </div>
+        <div className="flex gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-rows-3 gap-4">
+            <div className="group container relative mx-auto flex h-[250px] w-[250px] cursor-pointer items-center justify-center gap-3 shadow-lg sm:h-[317px] sm:w-[317px]">
+              <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
+                <a href="#">
+                  <img
+                    className="rounded-t-lg"
+                    src="/public/course image.svg"
+                    alt=""
+                  />
+                </a>
+                <div className="px-5">
+                  <a href="#">
+                    <h5 className="text mb-2 font-bold dark:text-left  ">
+                      The Complete Nutrition Course
+                    </h5>
+                  </a>
+                  <p className="ratingcardtext mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    Bekele Sewasew
+                  </p>
+                  <div className="flex items-center space-x-1 text-justify ">
+                    <p className="ratingcardtext mb-3  font-normal text-gray-700 dark:text-gray-400">
+                      4.95
+                    </p>
+                    <div className="flex items-center">
+                      {[...Array(5)].map((_, index) => (
+                        <svg
+                          key={index}
+                          className="me-1 h-3 w-3 text-primary lg:h-4 lg:w-4"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                      ))}
+                    </div>
+                    <p className="ratingcardtext mb-3  font-normal text-gray-700 dark:text-gray-400">
+                      (105,623)
+                    </p>
+                  </div>
+                  <p className="phonetext mb-3 font-normal text-gray-700 dark:font-bold dark:text-gray-400">
+                    Birr 1,500
+                  </p>
+                </div>
+              </div>
+
+              <div className="hover-card invisible absolute right-0 z-10 -mx-2 h-auto w-full translate-x-full transform bg-white p-4 text-gray-900 opacity-0 transition-opacity duration-300 ease-in-out group-hover:visible group-hover:opacity-100 ">
+                <div className="mb-auto">
+                  <h5 className="text mb-2 font-bold dark:text-left">
+                    The Complete Nutrition Course
+                  </h5>
+                  <div className="flex items-center space-x-1 text-justify">
+                    <span className="cardtext text-gray-500 dark:text-gray-400">
+                      25 total hours
+                    </span>
+                    <span className="cardtext h-1 w-1 items-center justify-center rounded-full bg-gray-300 text-gray-500 dark:text-gray-400"></span>
+                    <span className="cardtext text-gray-500 dark:text-gray-400">
+                      All levels
+                    </span>
+                    <span className="ratingcardtext badge badge-primary badge-outline">
+                      VIRTUAL
+                    </span>
+                  </div>
+                </div>
+                <div className="gap-1 text-xs sm:gap-2 sm:text-base">
+                  <p className="cardtext">
+                    Unlock the secrets to optimal health with our Complete
+                    Nutrition Course, designed for individuals seeking a
+                    holistic understanding of nutrition.
+                  </p>
+                  <ul className="cardtext list-disc p-3">
+                    <li>
+                      Gain practical knowledge to make informed dietary choices
+                      and achieve your health goals.
+                    </li>
+                    <li>
+                      Explore the science of nutrition, covering essential
+                      nutrients, dietary patterns, and personalized meal
+                      planning.
+                    </li>
+                    <li>
+                      Acquire the skills to decipher food labels, assess
+                      nutritional needs, and implement sustainable lifestyle
+                      changes for lifelong well-being.
+                    </li>
+                  </ul>
+                </div>
+                <div className="card-actions  justify-end">
+                  <button className="buttontext btn btn-primary w-full">
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

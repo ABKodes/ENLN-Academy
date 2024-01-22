1ac1a188bd840e32b751ee83bb627baa3c70199f
BettyyN <betelhemnegash10@gmail.com>
2024-01-22 15:47:59 +0300

    lesson page width modified

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import Lessonsoftheweek from '../components/Lesson/Lessons of the week'
 import RightSideBar from '../components/Lesson/Right Side Bar'
 import LeftSideBar from "../components/Lesson/Left Side Bar";
+import HeaderLesson from '../components/Reusable Components/Header Lesson';
 
 function Lessons() {
   return (
     <div>
-      <div className='flex'>
-        <LeftSideBar />
-        <Lessonsoftheweek />
-        <RightSideBar />
+      <HeaderLesson />
+      <div className="flex w-full m-3">
+        <div className="w-1/4">
+          <LeftSideBar />
+        </div>
+        <div className="w-3/4">
+          <Lessonsoftheweek />
+        </div>
+        <div className="w-1/4">
+          <RightSideBar />
+        </div>
       </div>
     </div>
   );

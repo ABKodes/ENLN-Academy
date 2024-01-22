bdcf17ff8cecdcb19de17365ba13d6bd68b04223
BettyyN <betelhemnegash10@gmail.com>
2024-01-22 15:47:38 +0300

    configuration

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,13 @@ import Rating from "./components/Course Description/Rating";
 import OrganizationHome from "./pages/OrganizationHome";
 import Sampleforwhyus from "./components/Home Org/sampleforwhyus";
 import Lessons from "./pages/Lessons"
+import NoCourseFound from "./components/Reusable Components/No Course Found";
 function App() {
   return (
     <>
       <div className="overflow-x-hidden">
+        <NoCourseFound/>
+        <br />
         <Lessons/>
         <br />
         <OrganizationHome/>

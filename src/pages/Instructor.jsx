38c78712eaaebf3c0e33d2bc4c48086fca41d682
BettyyN <betelhemnegash10@gmail.com>
2024-01-24 11:12:49 +0300

    Instructor page configuration

---
+++ src/pages/Instructor.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MyCourses from '../components/Instructor/My Courses'
+import Profile from '../components/Instructor/Profile'
+import About from '../components/Instructor/About'
+
+function Instructor() {
+  return (
+    <div className='space-y-2'>
+        <Profile/>
+        <About/>
+        <MyCourses/>
+    </div>
+  )
+}
+
+export default Instructor

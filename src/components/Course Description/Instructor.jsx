4188bab3fd28cb1a7991cdd09e7324aa39b9eb69
ABKodes <abiyshiferaw12@gmail.com>
2024-01-26 09:27:07 +0300

    instructor

--- src/components/Course Description/Instructor.jsx
+++ src/components/Course Description/Instructor.jsx
@@ -3,7 +3,7 @@ import { FaLinkedinIn } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa6";
 function Instructor() {
   return (
-    <div className=" w-7/12 flex flex-col rounded-xl  bg-white p-4  md:p-6">
+    <div className=" flex w-7/12 flex-col rounded-xl  bg-white p-4  md:p-6">
       <div className="flex items-center gap-x-4">
         <img
           className="h-20 w-20 rounded-full"
@@ -31,12 +31,12 @@ function Instructor() {
               className="inline-flex h-8 w-8 items-center justify-center rounded-lg border border-secondary text-sm font-semibold text-secondary hover:bg-secondary hover:text-primary disabled:pointer-events-none disabled:opacity-50"
               href="#"
             >
-              <FaFacebookF  />
+              <FaFacebookF />
             </a>
           </div>
         </div>
       </div>
-      <p className="mt-3 text">
+      <p className="text mt-3">
         John is a software engineer with over 10 years of experience in
         developing web and mobile applications. He is skilled in JavaScript,
         React, and Node.js. John is a software engineer with over 10 years of

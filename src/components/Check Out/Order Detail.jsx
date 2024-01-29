bc9fcc2d53e57a1324eaedd7da16f9dbf86e0d23
ABKodes <abiyshiferaw12@gmail.com>
2024-01-29 15:59:31 +0300

    checkout

--- src/components/Check Out/Order Detail.jsx
+++ src/components/Check Out/Order Detail.jsx
@@ -209,6 +209,46 @@ function OrderDetail() {
           </button>
         </div>
       </div>
+
+      <div className="relative">
+        <ul className="space-y-5">
+          <li className="flex justify-between">
+            <div className="inline-flex">
+              <img
+                src="https://images.unsplash.com/photo-1620331311520-246422fd82f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGhhaXIlMjBkcnllcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                alt=""
+                className="max-h-16"
+              />
+              <div className="ml-3">
+                <p className="text-base font-semibold ">
+                  Nano Titanium Hair Dryer
+                </p>
+                <p className="text-sm font-medium  text-opacity-80">
+                  Pdf, doc Kindle
+                </p>
+              </div>
+            </div>
+            <p className="text-sm font-semibold ">$260.00</p>
+          </li>
+          <li className="flex justify-between">
+            <div className="inline-flex">
+              <img
+                src="/public/course image.svg"
+                alt=""
+                className="w-64"
+              />
+              <div className="ml-3">
+                <p className="text-base font-semibold ">The Complete Nutrition Course</p>
+                <p className="text-sm font-medium  text-opacity-80">
+                  Bekele Sewasew
+                </p>
+              </div>
+            </div>
+            <p className="text-sm font-semibold ">$350.00</p>
+          </li>
+        </ul>
+        <div className="my-5 h-0.5 w-full bg-white bg-opacity-30" />
+      </div>
     </>
   );
 }

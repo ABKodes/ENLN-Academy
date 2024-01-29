3c4ac60d64e7be000fa1d8ba3e6f7ee497bc48e5
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-01-29 10:32:50 -0800

    update the routing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,78 +3,18 @@
 import { Route, Routes } from "react-router-dom";
 import All from "./All";
 import Profile from "./components/Instructor/Profile";
-import About from "./components/Instructor/About";
-import MyCourses from "./components/Instructor/My Courses"
-import Instructors from "./pages/Instructor";
-import SignUp from "./pages/SignUp"
-import HeroI from "./components/Home Gov/Hero";
-import AddedCourses from "./components/Add to Cart/AddedCourses";
-import OrderDetail from "./components/Check Out/Order Detail";
+
 function App() {
   return (
     <>
       <div className="overflow-x-hidden">
-        <HeroI />
-        <br />
-        <Instructors />
-        <br />
-        <SignUp />
-        <br />
-        <NoCourseFound />
-        <br />
-        <Four0four />
-        <br />
-        <Lessons />
-        <br />
-        <OrganizationHome />
-        <br />
-        <AllRating />
-        <br />
-        <Sampleforwhyus />
-        <br />
-        <br />
-        <br />
-        <Categories />
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className="overflow-x-hidden">
-          <VerticalCard />
-        </div>
-        <ReviewModal />
-        <br />
-        <HeaderBefore />
-        <CourseHero />
-        <WhatYouLearn />
-        <CourseSidebar />
-        <CourseContent />
-        <CourseDetail />
-        <Instructor/>
-        <RatingCard />
-        <Rating />
-        <br />
-        <Login />
-        <HorizontalCard />
-        <AddedCourses/>
-        <OrderDetail/>
-        <br />
-        <ForgetPassword />
-        <HeaderBefore />
-        <Hero />
-        <Companies />
-        <WhatWeOffer />
-        <BroadSelection />
-        <Benefits />
-        <FAQ />
-        <Testimonial />
-        <RegisterToday />
-        <Footer />
+        <Routes>
+          <Route path="/" element={<All />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
       </div>
-      {/* <HorizontalCard /> */}
-      {/* <Companies /> */}
     </>
   );
-};
+}
 
 export default App;

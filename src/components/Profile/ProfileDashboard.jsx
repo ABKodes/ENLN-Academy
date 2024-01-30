3d5363356e01c16760b5b50e3cb2ab4463f78f8d
lebamlak <lbmlkamr@gmail.com>
2024-01-30 09:15:53 -0800

    dash update

--- src/components/Profile/ProfileDashboard.jsx
+++ src/components/Profile/ProfileDashboard.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./dash.css"
+import {
+    FaTh,
+    FaBars,
+    FaUserAlt,
+    FaRegChartBar,
+    FaCommentAlt,
+    FaCog,
+    FaThList
+}from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 
-const ProfileDashboard = () => {
-  return (
-    <div>ProfileDashboard</div>
-  )
-}
 
-export default ProfileDashboard
+const ProfileDashboard = ({children}) => {
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+    const menuItem=[
+        {
+            path:"/",
+            name:"Dashboard",
+            icon:<FaTh/>
+        },
+        {
+            path:"/My Courses",
+            name:"My Courses",
+            icon:<FaUserAlt/>
+        },
+        {
+            path:"/Certificate",
+            name:"Certificate",
+            icon:<FaRegChartBar/>
+        },
+        {
+            path:"/Support",
+            name:"Support",
+            icon:<FaCommentAlt/>
+        },
+        {
+            path:"/Settings",
+            name:"Settings",
+            icon:<FaCog/>
+        },
+        {
+            path:"/Logout",
+            name:"Logout List",
+            icon:<FaThList/>
+        }
+    ]
+    return (
+        <div className="container">
+           <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
+               <div className="top_section">
+                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
+                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                       <FaBars onClick={toggle}/>
+                   </div>
+               </div>
+               {
+                   menuItem.map((item, index)=>(
+                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                           <div className="icon">{item.icon}</div>
+                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                       </NavLink>
+                   ))
+               }
+           </div>
+           <main>{children}</main>
+        </div>
+    );
+};
+
+export default ProfileDashboard;

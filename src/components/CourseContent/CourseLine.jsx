2513e47a6eb52a10cefed0bc940a9bfdd09b1715
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-01-30 14:35:06 -0800

    finalize the course section and make it responsive

--- src/components/CourseContent/CourseLine.jsx
+++ src/components/CourseContent/CourseLine.jsx
@@ -2,11 +2,12 @@
 import { FaRegCirclePlay } from "react-icons/fa6";
 import { BsStopwatch } from "react-icons/bs";
 import { PiBookOpenTextBold } from "react-icons/pi";
+import { MdQuiz } from "react-icons/md";
 
 const CourseLine = () => {
   return (
     <div>
-      <div className="flex min-h-[80vh] gap-2 bg-[#B3CBD0] px-2 py-3 lg:gap-8">
+      <div className="flex min-h-[80vh] flex-col-reverse gap-2 bg-[#B3CBD0] px-2 py-3 lg:flex-row lg:gap-8">
         <div className="z-50 my-4 rounded border-[1px] border-gray-300 bg-white px-4 pb-10 pt-4 md:w-1/3 lg:w-1/5">
           <p className="merb mb-5 pt-2 text-[20px]">Course Progress</p>
           <div className="flex items-center justify-between border-b-2 border-gray-300 pb-5">
@@ -15,7 +16,7 @@ const CourseLine = () => {
           </div>
 
           <p className="amib my-3">Lessons</p>
-          <div className="flex flex-col items-center justify-start gap-5">
+          <div className="flex flex-col items-start justify-start gap-5">
             <div className="flex items-start justify-start gap-3">
               <FaRegCirclePlay className="mt-2" />
               <div>
@@ -30,7 +31,72 @@ const CourseLine = () => {
               <div>
                 <p className="amir text-start">What is Nutrition?</p>
                 <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
+                  Reading 10 min
+                </p>
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-3">
+              <MdQuiz className="mt-2" />
+              <div>
+                <p className="amir text-start">Practice Quiz</p>
+                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
+                  Quiz 10 min
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start justify-start gap-3">
+              <FaRegCirclePlay className="mt-2" />
+              <div>
+                <p className="amir text-start">Introduction to Nutrition</p>
+                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
                   Video 10 min
+                </p>
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-3">
+              <PiBookOpenTextBold className="mt-2" />
+              <div>
+                <p className="amir text-start">What is Nutrition?</p>
+                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
+                  Reading 10 min
+                </p>
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-3">
+              <MdQuiz className="mt-2" />
+              <div>
+                <p className="amir text-start">Practice Quiz</p>
+                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
+                  Quiz 10 min
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start justify-start gap-3">
+              <FaRegCirclePlay className="mt-2" />
+              <div>
+                <p className="amir text-start">Introduction to Nutrition</p>
+                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
+                  Video 10 min
+                </p>
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-3">
+              <PiBookOpenTextBold className="mt-2" />
+              <div>
+                <p className="amir text-start">What is Nutrition?</p>
+                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
+                  Reading 10 min
+                </p>
+              </div>
+            </div>
+            <div className="flex items-start justify-start gap-3">
+              <MdQuiz className="mt-2" />
+              <div>
+                <p className="amir text-start">Practice Quiz</p>
+                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
+                  Quiz 10 min
                 </p>
               </div>
             </div>
@@ -44,72 +110,27 @@ const CourseLine = () => {
               </div>
             </div>
             <div className="flex items-start justify-start gap-3">
-              <FaRegCirclePlay className="mt-2" />
+              <PiBookOpenTextBold className="mt-2" />
               <div>
-                <p className="amir text-start">Introduction to Nutrition</p>
+                <p className="amir text-start">What is Nutrition?</p>
                 <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
-                  Video 10 min
+                  Reading 10 min
                 </p>
               </div>
             </div>
             <div className="flex items-start justify-start gap-3">
-              <FaRegCirclePlay className="mt-2" />
+              <MdQuiz className="mt-2" />
               <div>
-                <p className="amir text-start">Introduction to Nutrition</p>
+                <p className="amir text-start">Practice Quiz</p>
                 <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
-                  Video 10 min
-                </p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start gap-3">
-              <FaRegCirclePlay className="mt-2" />
-              <div>
-                <p className="amir text-start">Introduction to Nutrition</p>
-                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
-                  Video 10 min
-                </p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start gap-3">
-              <FaRegCirclePlay className="mt-2" />
-              <div>
-                <p className="amir text-start">Introduction to Nutrition</p>
-                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
-                  Video 10 min
-                </p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start gap-3">
-              <FaRegCirclePlay className="mt-2" />
-              <div>
-                <p className="amir text-start">Introduction to Nutrition</p>
-                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
-                  Video 10 min
-                </p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start gap-3">
-              <FaRegCirclePlay className="mt-2" />
-              <div>
-                <p className="amir text-start">Introduction to Nutrition</p>
-                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
-                  Video 10 min
-                </p>
-              </div>
-            </div>
-            <div className="flex items-start justify-start gap-3">
-              <FaRegCirclePlay className="mt-2" />
-              <div>
-                <p className="amir text-start">Introduction to Nutrition</p>
-                <p className="-mt-1 text-start text-[14px] text-[#7E7E7E]">
-                  Video 10 min
+                  Quiz 10 min
                 </p>
               </div>
             </div>
           </div>
         </div>
         <div className="z-20 my-4 flex flex-col justify-start rounded border-[1px] border-gray-300 bg-white px-4 pb-10 pt-4  sm:px-4 md:w-2/3 md:px-8 lg:w-4/5">
-          <p className="text-[#445D6E]">
+          <p className="text-[16px] text-[#445D6E]">
             Home &gt; Nutrition Leadership &gt; Introduction
           </p>
           <div className="flex items-center justify-between py-5">
@@ -129,14 +150,14 @@ const CourseLine = () => {
           </p>
           <div className="flex flex-col items-center justify-center">
             <iframe
-              width="853"
-              height="480"
+              // width="853"
+              // height="480"
               src="https://www.youtube.com/embed/Ovry6x71gxE"
               title="Dr Iftekhar Rashid | Keynote Speech | Ethiopian Nutrition Leaders Network - ENLN | 2023 Annual Forum"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
-              className="mt-10 lg:min-h-[458.44px] lg:min-w-[815px]"
+              className="mt-10 h-[458.44px] w-screen lg:min-w-[815px]"
             ></iframe>
           </div>
         </div>

6d7460b9d6a2617f2be15a06ec71c3b18b50dd1d
ABKodes <abiyshiferaw12@gmail.com>
2024-01-31 10:24:08 +0300

    Individual Home Page

---
+++ src/pages/Ind Home Page.jsx
@@ -0,0 +1,24 @@
+import Hero from '../components/Home Ind/Hero';
+import Companies from '../components/Home Ind/Companies';
+import WhatWeOffer from '../components/Home Ind/WhatWeOffer';
+import BroadSelection from '../components/Home Ind/BroadSelection';
+import Benefits from '../components/Home Ind/Benefits';
+import FAQ from '../components/Home Ind/FAQ';
+import Testimonial from '../components/Home Ind/Testimonial';
+import RegisterToday from '../components/Home Ind/RegisterToday';
+function IndHomePage() {
+  return (
+    <div>
+      <Hero />
+      <Companies />
+      <WhatWeOffer />
+      <BroadSelection />
+      <Benefits />
+      <FAQ />
+      <Testimonial />
+      <RegisterToday />
+    </div>
+  );
+}
+
+export default IndHomePage

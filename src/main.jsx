bafd3a7c10d5cb2cddb37685d9bc6650dbfbb0c5
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-01-31 11:38:02 -0800

    add some packages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import "alpinejs";
 import { BrowserRouter } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

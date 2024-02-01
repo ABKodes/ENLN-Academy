35355a0eecfff4af682484ca1e42c8e9d375b817
BettyyN <betelhemnegash10@gmail.com>
2024-02-01 09:42:44 +0300

    configuration change

--- src/pages/Instructor.jsx
+++ src/pages/Instructor.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import MyCourses from '../components/Instructor/My Courses'
 import Profile from '../components/Instructor/Profile'
 import About from '../components/Instructor/About'
-import HeaderBefore from '../components/Reusable Components/HeaderBefore'
-import Footer from '../components/Reusable Components/Footer'
 
 function Instructor() {
   return (
     <div className='space-y-2'>
-      <HeaderBefore/>
         <Profile/>
         <About/>
         <MyCourses/>
-        <Footer/>
     </div>
   )
 }

9f8edf6ba15aa1d444efa2d83d0b9538e84ea219
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-02-01 11:49:26 -0800

    add filter method and put values

--- src/components/Profile/MyCourses.jsx
+++ src/components/Profile/MyCourses.jsx
@@ -15,51 +15,99 @@ const MyCourses = () => {
           Completed Courses
         </p>
       </div>
+     {active ? (
+       <div className="-mt-10 flex flex-col items-center justify-center">
+       <div className="flex flex-col items-center justify-center  ">
+         <div className="grid grid-cols-1 gap-4 p-2 text-center sm:p-4 md:grid-cols-2 md:p-12 lg:grid-cols-3">
+           <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
+             <img src={tom} alt="" className="h-[186px] w-[250px]" />
+             <p className="amir text-[16px] font-semibold text-black">
+               The complete nutrition course
+             </p>
+             <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
+             <p className="py-1 text-start font-bold text-[#187498]">
+               Completed
+             </p>
+             <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
+               Download Certificate
+             </p>
+           </div>
+           <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
+             <img src={tom} alt="" className="h-[186px] w-[250px]" />
+             <p className="amir text-[16px] font-semibold text-black">
+               The complete nutrition course
+             </p>
+             <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
+             <p className="py-1 text-start font-bold text-[#187498]">
+               Completed
+             </p>
+             <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
+               Download Certificate
+             </p>
+           </div>
+           <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
+             <img src={tom} alt="" className="h-[186px] w-[250px]" />
+             <p className="amir text-[16px] font-semibold text-black">
+               The complete nutrition course
+             </p>
+             <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
+             <p className="py-1 text-start font-bold text-[#187498]">
+               Completed
+             </p>
+             <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
+               Download Certificate
+             </p>
+           </div>
+         </div>
+       </div>
+     </div>
+     ):(
       <div className="-mt-10 flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center  ">
-          <div className="grid grid-cols-1 gap-4 p-2 text-center sm:p-4 md:grid-cols-2 md:p-12 lg:grid-cols-3">
-            <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
-              <img src={tom} alt="" className="h-[186px] w-[250px]" />
-              <p className="amir text-[16px] font-semibold text-black">
-                The complete nutrition course
-              </p>
-              <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
-              <p className="py-1 text-start font-bold text-[#187498]">
-                Completed
-              </p>
-              <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
-                Download Certificate
-              </p>
-            </div>
-            <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
-              <img src={tom} alt="" className="h-[186px] w-[250px]" />
-              <p className="amir text-[16px] font-semibold text-black">
-                The complete nutrition course
-              </p>
-              <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
-              <p className="py-1 text-start font-bold text-[#187498]">
-                Completed
-              </p>
-              <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
-                Download Certificate
-              </p>
-            </div>
-            <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
-              <img src={tom} alt="" className="h-[186px] w-[250px]" />
-              <p className="amir text-[16px] font-semibold text-black">
-                The complete nutrition course
-              </p>
-              <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
-              <p className="py-1 text-start font-bold text-[#187498]">
-                Completed
-              </p>
-              <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
-                Download Certificate
-              </p>
-            </div>
+      <div className="flex flex-col items-center justify-center  ">
+        <div className="grid grid-cols-1 gap-4 p-2 text-center sm:p-4 md:grid-cols-2 md:p-12 lg:grid-cols-3">
+          <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
+            <img src={tom} alt="" className="h-[186px] w-[250px]" />
+            <p className="amir text-[16px] font-semibold text-black">
+              The complete nutrition course
+            </p>
+            <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
+            <p className="py-1 text-start font-bold text-[#187498]">
+              Completed
+            </p>
+            <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
+              Download Certificate
+            </p>
+          </div>
+          <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
+            <img src={tom} alt="" className="h-[186px] w-[250px]" />
+            <p className="amir text-[16px] font-semibold text-black">
+              The complete nutrition course
+            </p>
+            <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
+            <p className="py-1 text-start font-bold text-[#187498]">
+              Completed
+            </p>
+            <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
+              Download Certificate
+            </p>
+          </div>
+          <div className="flex h-full min-h-[300px] min-w-[250px] flex-col rounded-xl border-[1px] border-gray-400 px-2 pb-3 text-black">
+            <img src={tom} alt="" className="h-[186px] w-[250px]" />
+            <p className="amir text-[16px] font-semibold text-black">
+              The complete nutrition course
+            </p>
+            <p className="py-1 text-start text-[#7E7E7E]">Bekele Sewasew</p>
+            <p className="py-1 text-start font-bold text-[#187498]">
+              Completed
+            </p>
+            <p className="bg-[#025464] px-3 py-1 font-semibold text-white">
+              Download Certificate
+            </p>
           </div>
         </div>
       </div>
+    </div>
+     )}
     </div>
   );
 };

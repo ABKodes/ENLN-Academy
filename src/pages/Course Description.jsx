af273e623bc57834fe0a2ccd1d397b8a2b1434c8
ABKodes <abiyshiferaw12@gmail.com>
2024-02-02 09:57:12 +0300

    course description

---
+++ src/pages/Course Description.jsx
@@ -0,0 +1,13 @@
+import CourseContent from "../components/Course Description/CourseContent"
+import CourseDetail from "../components/Course Description/CourseDetail"
+import CourseHero from "../components/Course Description/CourseHero"
+import CourseSidebar from "../components/Course Description/CourseSidebar"
+import Instructor from "../components/Course Description/Instructor"
+import 
+function CourseDescription() {
+  return (
+    <div>Course Description</div>
+  )
+}
+
+export default CourseDescription

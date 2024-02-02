0741d46b4c615ee6403a25e819d01669ba70e8bd
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-02-02 10:27:06 -0800

    update gov form from page to components

--- src/pages/Course Description.jsx
+++ src/pages/Course Description.jsx
@@ -1,9 +1,10 @@
-import CourseContent from "../components/Course Description/CourseContent"
-import CourseDetail from "../components/Course Description/CourseDetail"
-import CourseHero from "../components/Course Description/CourseHero"
-import CourseSidebar from "../components/Course Description/CourseSidebar"
-import Instructor from "../components/Course Description/Instructor"
-import 
+// import CourseContent from "../components/Course Description/CourseContent"
+// import CourseDetail from "../components/Course Description/CourseDetail"
+// import CourseHero from "../components/Course Description/CourseHero"
+// import CourseSidebar from "../components/Course Description/CourseSidebar"
+// import Instructor from "../components/Course Description/Instructor"
+
+
 function CourseDescription() {
   return (
     <div>Course Description</div>

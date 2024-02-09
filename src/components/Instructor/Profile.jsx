87743c02b38d69c1c308dbe4cbf794f0ee8e9e74
BettyyN <betelhemnegash10@gmail.com>
2024-02-09 14:54:28 +0300

    changes on pages

--- src/components/Instructor/Profile.jsx
+++ src/components/Instructor/Profile.jsx
@@ -7,15 +7,15 @@ import CourseInstructor from "/public/course instructor.avif"
 function Profile() {
   const [value, setValue] = useState("");
   return (
-    <div className="">
-      <div className="my-10 w-9/12 rounded-lg mx-auto bg-white lg:p-5 ">
+    <div className="dark:bg-gray-800 dark:text-white">
+      <div className="mx-auto my-10 w-9/12 rounded-lg bg-white dark:bg-gray-800 lg:p-5 ">
         <div className="lg:flex">
           <img
             className="h-36 w-36 rounded-full"
             src={CourseInstructor}
             alt="Profile picture"
           />
-          <div className="lg:mx-5 text-left">
+          <div className="text-left lg:mx-5">
             <h2 className="mt-3 text-xl font-semibold">John Doe</h2>
             <p className="mt-1  text-gray-600">
               Nutiritionist, Public Health Worker

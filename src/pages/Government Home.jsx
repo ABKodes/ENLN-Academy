90f8cb0f3d75e44120805bf3e0d67f123deb7c4a
BettyyN <betelhemnegash10@gmail.com>
2024-02-11 18:29:46 +0300

    config

--- src/pages/Government Home.jsx
+++ src/pages/Government Home.jsx
@@ -1,6 +1,5 @@
 // import React from 'react'
 import Hero from "../components/Home Gov/Hero"
-// import Partners from "../components/Home Gov/Partners";
 import WhyUs from "../components/Home Gov/Why Us";
 import GovHub from "../components/Home Gov/Gov Hub";
 import Testimonials from "../components/Home Ind/Testimonial"
@@ -11,7 +10,6 @@ function GovernmentHome() {
     <div>
       <Hero />
       <Companies />
-      {/* <Partners />  */}
       <WhyUs />
       <GovHub />
       <Testimonials />

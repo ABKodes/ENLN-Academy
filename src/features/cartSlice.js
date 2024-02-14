f8a4c823e1a64e51675a3a844d83889c94b858d5
BettyyN <betelhemnegash10@gmail.com>
2024-02-14 14:05:32 +0300

    changes on cart slice

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -6,9 +6,18 @@ const cartSlice = createSlice({
   name: "courses",
   initialState,
   reducers: {
-    add(state, action) {
-      state.push(action.payload);
-    },
+   add(state, action) {
+  const courseToAdd = action.payload;
+  const existingCourseIndex = state.findIndex(course => course.id === courseToAdd.id);
+
+  if (existingCourseIndex === -1) {
+    // Course not found in the cart, add it
+    return [...state, courseToAdd];
+  } else {
+    return state;
+  }
+},
+
     // remove reducers 
     remove(state, action){
         return state.filter(item => item.id !== action.payload);

663fc4428cec0f04c2c0f451008416463b2320d5
ABKodes <abiyshiferaw12@gmail.com>
2024-02-14 09:33:03 +0300

    configuration

--- src/All.jsx
+++ src/All.jsx
@@ -38,13 +38,12 @@ import PrivacyAndPolicy from "./pages/PrivacyAndPolicy";
 import AboutUs from "./pages/About Us";
 import HeaderGov from "./components/Reusable Components/Header Gov";
 // import HeaderOrg from "./components/Reusable Components/Header Org";
-import LogOut from "./components/Reusable Components/Log Out";
+
 
 function All() {
   return (
     <>
       <div className="overflow-x-hidden">
-        <LogOut/>
         <HeaderLesson />
         <HeaderGov />
         {/* <HeaderOrg /> */}

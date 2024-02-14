26c281704ce4e9cac391b963f61fa64489f13dc5
BettyyN <betelhemnegash10@gmail.com>
2024-02-14 09:34:54 +0300

    redux provider add to mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,16 @@ import "alpinejs";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ToastContainer />
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider >
     </BrowserRouter>
   </React.StrictMode>,
 );

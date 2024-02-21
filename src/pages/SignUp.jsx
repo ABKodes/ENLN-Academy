d0ec14680040402bb6734005437c95e8bd532fd7
BettyyN <betelhemnegash10@gmail.com>
2024-02-21 11:41:32 +0300

    social media integration

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,12 +7,35 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { LoginSocialFacebook } from "reactjs-social-login";
 import { FacebookLoginButton } from "react-social-login-buttons";
+import google from "../assets/google.png";
+import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
+
 function SignUp() {
   const [open, setOpen] = useState(false);
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [p, setp] = useState("");
+   const [googleProfile, setgoogleProfile] = useState("");
+   const login = useGoogleLogin({
+     onSuccess: async (response) => {
+       try {
+         const res = await axios.get(
+           "https://www.googleapis.com/oauth2/v3/userinfo",
+           {
+             headers: { Authorization: `Bearer ${response.access_token}` },
+           },
+         );
+         console.log(res);
+         setgoogleProfile(res.data);
+       } catch (err) {
+         console.log(err);
+       }
+     },
+   });
 
   const navigate = useNavigate();
   // handle toggle
@@ -22,7 +45,7 @@ function SignUp() {
  
   
   const validateForm = ()  => {
-    if (!(p.name ||name) || !(p.email || email) || !password) {
+    if (!(p.name || name || googleProfile.name) || !(p.email || email || googleProfile.email) || !password) {
       toast.error("Please fill in all fields.");
       return false;
     }
@@ -38,7 +61,11 @@ function SignUp() {
       return;
     }
 
-    let regobj = { name, email, password };
+    let regobj = {
+      name: p ? p.name : googleProfile ? googleProfile.name : name,
+      email: p ? p.email : googleProfile ? googleProfile.email : email,
+      password: password,
+    };
     // console.log(regobj);
 
     fetch("http://localhost:3001/users", {
@@ -97,8 +124,18 @@ function SignUp() {
                       name="fullname"
                       className="peer h-10 w-full rounded-md border-none bg-transparent px-2 py-[0.32rem] text-gray-900 placeholder-transparent ring-1 ring-secondary focus:outline-none focus:ring-1 focus:ring-primary"
                       placeholder="Full Name"
-                      value={p ? p.name : name}
-                      onChange={(e) => setName(p ? p.name : e.target.value)}
+                      value={
+                        p ? p.name : googleProfile ? googleProfile.name : name
+                      }
+                      onChange={(e) =>
+                        setName(
+                          p
+                            ? p.name
+                            : googleProfile
+                              ? googleProfile.name
+                              : e.target.value,
+                        )
+                      }
                     />
                     <label
                       htmlFor="fullname"
@@ -117,8 +154,22 @@ function SignUp() {
                       name="email"
                       className="peer h-10 w-full rounded-md border-none bg-transparent px-2 py-[0.32rem] text-gray-900 placeholder-transparent ring-1 ring-secondary focus:outline-none focus:ring-1 focus:ring-primary"
                       placeholder="Email"
-                      value={p ? p.email : email}
-                      onChange={(e) => setEmail(p ? p.email : e.target.value)}
+                      value={
+                        p
+                          ? p.email
+                          : googleProfile
+                            ? googleProfile.email
+                            : email
+                      }
+                      onChange={(e) =>
+                        setEmail(
+                          p
+                            ? p.email
+                            : googleProfile
+                              ? googleProfile.email
+                              : e.target.value,
+                        )
+                      }
                     />
                     <label
                       htmlFor="email"
@@ -219,25 +270,20 @@ function SignUp() {
                     </a>{" "}
                   </LoginSocialFacebook>
                 </div>
-                <a
-                  className="hover:bg-info-600 focus:bg-info-600 active:bg-info-700 mb-3 flex w-full items-center justify-center rounded bg-info px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]"
-                  style={{ backgroundColor: "#55acee" }}
-                  href="#!"
-                  role="button"
-                  data-te-ripple-init=""
-                  data-te-ripple-color="light"
+
+                <div
+                  className="flex min-w-[300px] flex-row items-center justify-center rounded border-2 border-gray-100 bg-white px-2 py-2"
+                  onClick={() => login()}
                 >
-                  {/* Twitter */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mr-2 h-3.5 w-3.5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                  </svg>
-                  Continue with Twitter
-                </a>
+                  <img
+                    src={google}
+                    alt="Google login"
+                    className="h-[25px] w-[25px]"
+                  />
+                  <p className="amib pl-4 text-base font-bold text-black">
+                    Continue With Google
+                  </p>
+                </div>
               </form>
               <div className="mb-6 flex items-center justify-center space-x-2">
                 {/* Forgot password link */}

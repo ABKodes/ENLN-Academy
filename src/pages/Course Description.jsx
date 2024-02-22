001de7f1bf30e9e518814f28133aff29f61674cc
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-22 08:56:04 +0300

    comment some

--- src/pages/Course Description.jsx
+++ src/pages/Course Description.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import CourseContent from "../components/Course Description/CourseContent";
 import CourseDetail from "../components/Course Description/CourseDetail";
 import CourseHero from "../components/Course Description/CourseHero";
-import CourseSidebar from "../components/Course Description/CourseSidebar";
+// import CourseSidebar from "../components/Course Description/CourseSidebar";
 import Instructor from "../components/Course Description/Instructor";
 import WhatYouLearn from "../components/Course Description/WhatYouLearn";
 import RatingCard from "../components/Reusable Components/Rating Card";

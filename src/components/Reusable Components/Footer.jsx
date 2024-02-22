790d251ffbe6fab248a24bc1936c501210813a40
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-22 13:25:27 +0300

    update the whole footer section

--- src/components/Reusable Components/Footer.jsx
+++ src/components/Reusable Components/Footer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { PiPhonePlusThin } from "react-icons/pi";
 import { CiMail } from "react-icons/ci";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -18,10 +24,12 @@ const Footer = () => {
               />
             </div>
             <div>
-              <p className="hover:border-b-[1px] border-[#B3CBD0] font-normal">
+              <p className="border-[#B3CBD0] font-normal duration-500 ease-in hover:border-b-[1px]">
                 +2519781684
               </p>
-              <p className="text-[14px] font-normal hover:border-b-[1px] border-[#B3CBD0] pt-1">+2519781684</p>
+              <p className="border-[#B3CBD0] pt-1 text-[14px] font-normal duration-500 ease-in hover:border-b-[1px]">
+                +2519781684
+              </p>
             </div>
           </div>
           <div className="mt-5 flex items-center justify-center gap-3">
@@ -35,11 +43,52 @@ const Footer = () => {
         </div>
         <div className="flex flex-col items-start justify-start gap-4">
           <p className="amib py-3 text-center text-[16px] font-semibold md:text-[18px] lg:text-[20px] xl:text-[24px] xl:font-light">
-          OUR  INFORMATION
+            OUR INFORMATION
+          </p>
+          <p className="border-[#B3CBD0] text-[14px] font-medium duration-500 ease-in hover:border-b-[1px]">
+            ABOUT US
+          </p>
+          <p className="mt-1 border-[#B3CBD0] text-[14px] font-medium duration-500 ease-in hover:border-b-[1px]">
+            HELP
+          </p>
+          <p className="mt-1 border-[#B3CBD0] text-[14px] font-medium duration-500 ease-in hover:border-b-[1px]">
+            PRIVACY
+          </p>
+          <p className="mt-1 border-[#B3CBD0] text-[14px] font-medium duration-500 ease-in hover:border-b-[1px]">
+            TERMS AND CONDITIONS
           </p>
         </div>
-        <div></div>
-        <div></div>
+        <div className="flex flex-col items-start justify-start gap-4">
+          <p className="amib py-3 text-center text-[16px] font-semibold md:text-[18px] lg:text-[20px] xl:text-[24px] xl:font-light">
+            CATAGORIES
+          </p>
+          <p className="border-[#B3CBD0] text-[14px] font-medium duration-500 ease-in hover:border-b-[1px]">
+            FOR INDIVIDUALS
+          </p>
+          <p className="mt-1 border-[#B3CBD0] text-[14px] font-medium duration-500 ease-in hover:border-b-[1px]">
+            FOR GOVERNMENT
+          </p>
+          <p className="mt-1 border-[#B3CBD0] text-[14px] font-medium duration-500 ease-in hover:border-b-[1px]">
+            FOR UNIVERSITY
+          </p>
+        </div>
+        <div className="flex flex-col items-start justify-start gap-4">
+          <p className="amib ml-3 py-3 text-center text-[16px] font-semibold md:text-[18px] lg:text-[20px] xl:text-[24px] xl:font-light">
+            FOLLOW US
+          </p>
+          <div className="-mt-4 ml-5 flex flex-col items-center justify-center gap-2">
+            <div className="flex gap-2">
+              <FaInstagramSquare size={30} className="mx-2 text-[#025464]" />
+              <FaTelegram size={30} className="mx-2 text-[#025464]" />
+              <FaTwitter size={30} className="mx-2 text-[#025464]" />
+            </div>
+            <div className="flex gap-2">
+              <FaFacebook size={30} className="mx-2 text-[#025464]" />
+              <FaLinkedin size={30} className="mx-2 text-[#025464]" />
+              <FaYoutube size={30} className="mx-2 text-[#025464]" />
+            </div>
+          </div>
+        </div>
       </div>
       <div>
         <div></div>

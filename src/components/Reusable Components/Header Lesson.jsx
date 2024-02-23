802c769c0d9379af3396b8a9f72a488ce2eecf61
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-23 11:24:38 +0300

    clear comments

--- src/components/Reusable Components/Header Lesson.jsx
+++ src/components/Reusable Components/Header Lesson.jsx
@@ -28,15 +28,7 @@ function HeaderLesson() {
     setIsMenuOpen(false);
   };
 
-  // const toggleMenu = () => {
-  //   setIsOpen(!isOpen);
-  // };
 
-  
-
-  // const closeMenu = () => {
-  //   setIsOpen(false);
-  // };
 
   const handleSearchBarClick = () => {
     setIsClicked(true);
@@ -62,72 +54,6 @@ function HeaderLesson() {
     <div className="bg-slate-300">
       <div className="navbar mx-auto w-11/12 ">
         <div className="navbar-start">
-          {/* Mobile hamburger */}
-          {/* <div className="dropdown">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost lg:hidden"
-              onClick={toggleMenu}
-            >
-              {isOpen ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-white"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="#025464"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-white"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="#025464"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </div>
-            <div
-              className={`absolute inset-x-0  w-full  px-6 py-2 transition-all duration-300 ease-in-out md:relative md:top-0 md:mt-0 md:flex md:w-auto md:translate-x-0 md:items-center md:bg-transparent md:p-0 md:opacity-100 ${
-                isOpen
-                  ? "translate-x-0 opacity-100"
-                  : "-translate-x-full opacity-0"
-              }`}
-            >
-              <ul
-                tabIndex={0}
-                className="menu dropdown-content menu-sm z-[] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
-              >
-                <li>
-                  <Link className="group relative  text-primary hover:font-bold">
-                    Notification
-                    <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-primary transition-transform group-hover:scale-x-100"></span>
-                  </Link>
-                </li>
-                <li>
-                  <Link className="group relative text-primary hover:font-bold">
-                    Support
-                    <span className="absolute inset-x-0 bottom-0 h-0.5 origin-left scale-x-0 transform bg-primary transition-transform group-hover:scale-x-100"></span>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div> */}
           <nav className="relative mx-auto flex w-11/12  items-center justify-between px-4 py-2">
             <div className="lg:hidden">
               <button
@@ -212,80 +138,6 @@ function HeaderLesson() {
               </div>
             </button>
           </div>
-          {/*   Mobile search icon */}
-          {/* <div className="flex lg:hidden">
-              <div>
-                <button className="btn btn-circle btn-ghost text-primary">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6  cursor-pointer"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    onClick={toggleSearch}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                </button>
-              </div>
-
-              {isSearchVisible && (
-                <div className=" lg:hidden ">
-                  <div className=" duration-0 ">
-                    <div className="relative top-0 ">
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <svg
-                          className={`h-5 w-5 duration-0 ${
-                            isClicked ? "text-primary" : "text-gray-400"
-                          }`}
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke={isClicked ? "#025464" : "#b3cbd0"}
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" />
-                        </svg>
-                        <div
-                          tabIndex={0}
-                          role="button"
-                          className="btn btn-ghost lg:hidden"
-                          onClick={toggleeMenu}
-                        > 
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6 text-white "
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="#025464"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M6 18L18 6M6 6l12 12"
-                            />
-                          </svg>
-                        </div> 
-                      </span>
-                      <input
-                        type="text"
-                        onClick={handleSearchBarClick}
-                        onBlur={handleSearchBarBlur}
-                        className="w-full rounded-md border bg-white py-2 pl-10 pr-40 placeholder-gray-400 focus:border-primary focus:placeholder-primary focus:outline-none dark:border-secondary dark:text-gray-800"
-                        placeholder="Search in Course"
-                      />
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div> */}
           <div className=" right-16 flex lg:hidden">
             <div className="w-">
               <button
